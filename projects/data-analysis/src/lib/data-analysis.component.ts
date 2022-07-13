e41e49bb9a434fad138810aea78b2459a4f06246
Cait Bailey <cait@mdibl.org>
2022-07-13 12:53:45 -0400

    Added some query stuff

--- projects/data-analysis/src/lib/data-analysis.component.ts
+++ projects/data-analysis/src/lib/data-analysis.component.ts
@@ -1,28 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { ObservationApiService } from 'anecdata-ngcore';
+import { RowQueryData } from 'anecdata-ngcore/lib/interfaces/observations/RowQueryData';
+import { RowResultData } from 'anecdata-ngcore/lib/interfaces/observations/RowResultData';
 
-import { map, Observable, Subject } from 'rxjs';
+import {
+  BehaviorSubject,
+  debounceTime,
+  map,
+  Observable,
+  shareReplay,
+  Subject,
+  switchMap
+} from 'rxjs';
 @Component({
   selector: 'lib-data-analysis',
-  template: `
-    <p>
-      Data-analysis works! {{((posts | async) ?? []).length}} observations loaded from server.
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './data-analysis.component.html',
+  styleUrls: ['./data-analysis.component.scss'],
 })
-export class DataAnalysisComponent  {
+export class DataAnalysisComponent {
+  @Input() query: RowQueryData = {};
 
+  rows: Observable<RowResultData[]> = new Subject();
+  fields: Observable<RowResultData['Field'][]> = new Subject();
 
-  posts: Observable<any[]> = new Subject();
+  // Observable of all query changes. Everything listens to this.
+  queryChanges: BehaviorSubject<RowQueryData> = new BehaviorSubject({});
 
-  constructor(
-    private obsApi: ObservationApiService
-  ) { }
+  destroy = new Subject<void>();
+
+  constructor(private obsApi: ObservationApiService) {}
 
   ngOnInit(): void {
-    this.posts = this.obsApi.list({}).pipe(map(res => res.posts));
+
+    this.queryChanges.next(this.query);
+
+    this.rows = this.queryChanges.pipe(
+      debounceTime(200),
+      switchMap((query) => this.obsApi.listRows(query)),
+      map((res) => res.rows),
+      shareReplay(1)
+    );
+
+    this.fields = this.rows.pipe(
+      map((res) => res.map((r) => r.Field)),
+      shareReplay(1)
+    );
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['query']) {
+      this.queryChanges.next(this.query);
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroy.next();
+    this.destroy.complete();
+  }
 }
